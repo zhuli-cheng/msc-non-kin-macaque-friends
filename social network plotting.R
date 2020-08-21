ce89483c2e578aa5fc98ff7c7db1876e49a7ed40
zhuli cheng <zhulicheng@zhulidembp.home>
2020-08-21 15:19:08 +0100

    updates

--- social network plotting.R
+++ social network plotting.R
@@ -1,27 +1,53 @@
+rm(list=ls())
 library(reshape)
 library(igraph)
 library(dplyr)
 
-sna<-
-rm(list=ls())
-F2010sna<-read.csv("~/Desktop/Non-kin cooperation data/wrangled data/f2010DSI.csv",header=T)%>%
-  select(c("focal.id","partner.id","DSI"))
+groupyears <-
+  c( "f2010",
+     "f2011",
+     "f2012",
+     "f2013",
+     "f2014",
+     "f2015",
+     "f2016",
+     "f2017",
+     "hh2014",
+     "hh2016",
+     "kk2013",
+     "kk2015",
+     "kk2017",
+     "r2015",
+     "r2016",
+     "s2011",
+     "v2015",
+     "v2016",
+     "v2017"
+  )
+
+sna<-function (i) {
+  gysna<-read.csv(file=paste0("~/Desktop/Non-kin cooperation data/wrangled data/",i,"DSI.csv"),header=T) %>%
+                    select(c("focal.id","partner.id","DSI")) %>%
+    mutate(DSI=as.numeric(DSI))
+  
+  gysna<-as.matrix(cast(gysna, focal.id~partner.id,margins=c("partner.id","focal.id")))
+  
+  gysna[is.na(gysna)] <- 0
+  
+  monkey.matrix=graph.adjacency(gysna,mode="undirected",weighted=T)
+  
+  V(monkey.matrix)$label.cex <- 0.5
+  
+  pdf(paste0("~/Desktop/Non-kin cooperation data/graphs/",i,"sna.pdf"))
+  sociogram3 <- plot.igraph(monkey.matrix, vertex.size=3,
+                            edge.width=E(monkey.matrix)$weight*0.1,
+                            edge.color="black", edge.arrow.size = 0.5)
+  dev.off()
+  
+}
 
 
-F2010sna$groom.giving<-as.numeric(F2010sna$DSI)
 
-F2010sna<-as.matrix(cast(F2010sna, focal.id~partner.id,margins=c("partner.id","focal.id")))
-
-F2010sna[is.na(F2010sna)] <- 0
-
-monkey.matrix=graph.adjacency(F2010sna,mode="undirected",weighted=T) # this will create an directed 'igraph object'. Change qualifiers to make "undirected" or unweighted (null)
-
-#changes size of labels of vertex (nodes)
-V(monkey.matrix)$label.cex <- 1
-
-#vary edge width by strength of association (weighted)
-sociogram3 <- plot.igraph(monkey.matrix, vertex.size=3,
-                          edge.width=E(monkey.matrix)$weight*0.1,
-                          edge.color="black", edge.arrow.size = 0.5)
+lapply(groupyears, sna)
 
 
